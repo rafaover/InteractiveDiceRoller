7fabbdb91284f6e473514b0ac0cd598d188dd0c2
rafaover <rafamsilva@gmail.com>
2022-11-17 12:05:14 +1100

    Dice Class+Fun RollDice

--- app/src/main/java/com/example/interactivediceroller/MainActivity.kt
+++ app/src/main/java/com/example/interactivediceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.interactivediceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 
@@ -11,10 +12,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-
         rollButton.setOnClickListener {
-            //val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            Snackbar.make(rollButton, "Dice Rolled!", Snackbar.LENGTH_SHORT).show()
+            rollDice()
+            //val resultTextView: TextView = findViewById(R.id.textView)
+            //resultTextView.text = "6"
+
+            // val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT) -- DEPRECATED
+            // For future codes, using SnackBar is recommended by google via Material Design.
+            Snackbar.make(rollButton, "WELL DONE", Snackbar.LENGTH_SHORT).show()
         }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
     }
 }
