1ff5b40b1208d71fc313fee7c98a0f303cfcc0fa
rafaover <rafamsilva@gmail.com>
2022-11-17 11:22:03 +1100

    added snackbar

--- app/src/main/java/com/example/interactivediceroller/MainActivity.kt
+++ app/src/main/java/com/example/interactivediceroller/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.interactivediceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener {
+            //val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            Snackbar.make(rollButton, "Dice Rolled!", Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
