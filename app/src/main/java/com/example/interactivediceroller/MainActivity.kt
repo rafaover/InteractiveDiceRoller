509613b2fbe0cfb4820518a48f7ffbc535988206
rafaover <rafamsilva@gmail.com>
2022-11-18 13:14:48 +1100

    Add Dice Image+changeDice Face

--- app/src/main/java/com/example/interactivediceroller/MainActivity.kt
+++ app/src/main/java/com/example/interactivediceroller/MainActivity.kt
@@ -2,14 +2,10 @@ package com.example.interactivediceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 
-/**
- * This activity allows the user to roll a dice and view the result
- * on the screen.
- */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,15 +14,18 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
             rollDice()
+
             //val resultTextView: TextView = findViewById(R.id.textView)
             //resultTextView.text = "6"
-
             // val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT) -- DEPRECATED
+
             // For future codes, using SnackBar is recommended by google via Material Design.
-            val snackList = listOf(
-                "Well Done!", "Let's Roll", "Roll Again!", "Oh No!", "Fuck That", "Hell No!"
+
+            val snackListPos = listOf(
+                "Well Done!", "Let's Roll", "That's what you got", "Hell Yeah!", "Noice!"
             )
-            Snackbar.make(rollButton, snackList.random(), Snackbar.LENGTH_SHORT).show()
+            Snackbar.make(rollButton, snackListPos.random(), Snackbar.LENGTH_SHORT).show()
+
         }
     }
 
@@ -35,15 +34,17 @@ class MainActivity : AppCompatActivity() {
      */
     private fun rollDice() {
         val dice = Dice(20) // Calling Dice Class to create a D6
-        val diceRoll1 = dice.roll()
-        val diceRoll2 = dice.roll()
-
-
-        // Update the screen with the dice roll
-        val resultTextView1: TextView = findViewById(R.id.textView)
-        val resultTextView2: TextView = findViewById(R.id.textView2)
-        // Need to convert to string because of textView
-        resultTextView1.text = diceRoll1.toString()
-        resultTextView2.text = diceRoll2.toString()
+        val diceRoll = dice.roll()
+        // ID:Dice (ImageView)
+        val diceImage: ImageView = findViewById(R.id.dice)
+        // Update the result if the proper dice image
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
     }
 }
